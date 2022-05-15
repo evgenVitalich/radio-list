d3180e8a5d0eb01f59167200186107fb8cfdfe03
evgenVitalich <kharchenko_e_v@mail.ru>
2022-05-15 12:54:38 +0300

    Init commit

---
+++ src/main/java/evgenVitalich/radiolist/RadioListApplication.java
@@ -0,0 +1,13 @@
+package evgenVitalich.radiolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/evgenVitalich/radiolist/RadioListApplicationTests.java
@@ -0,0 +1,13 @@
+package evgenVitalich.radiolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadioListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
