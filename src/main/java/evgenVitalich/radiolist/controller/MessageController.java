59b35b3f8df0946bbca7b8e81d79a9dfd36a165f
evgenVitalich <kharchenko_e_v@mail.ru>
2022-05-15 13:02:44 +0300

    add hello controller

--- src/main/java/evgenVitalich/radiolist/controller/MessageController.java
+++ src/main/java/evgenVitalich/radiolist/controller/MessageController.java
@@ -1,12 +1,19 @@
 package evgenVitalich.radiolist.controller;
 
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping
+@RestController
+@RequestMapping("hello")
 public class MessageController {
+    @GetMapping
+    public String list(){
+
+    return "Привет, Настюшка!!!";
+
+    }
 
 
 }
